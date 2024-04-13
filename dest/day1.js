b31d806fd864ee766b9acac6fce77db070930eec
adilmalik01 <malikadil5679@gmai.com>
2024-04-13 22:04:01 +0500

    Day 1 compelete

--- dest/day1.js
+++ dest/day1.js
@@ -1,3 +1,19 @@
 "use strict";
+////////////////////////////////////////////    QUESTION 2  //////////////////////////////////////////
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Day 1");
+////////////////////////////////////////////     ANSWER  //////////////////////////////////////////
+let studentName = "Muhammad Adil";
+console.log(`“Hello ${studentName},would you like to learn some TypeScript today?”`);
+////////////////////////////////////////////    QUESTION 3  //////////////////////////////////////////
+//Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+////////////////////////////////////////////     ANSWER  //////////////////////////////////////////
+// change in lower Case
+let lowerCaseName = "ABDULLAH";
+console.log(lowerCaseName.toLowerCase());
+// change in upper Case
+let upperCaseName = "abdullah";
+console.log("", upperCaseName.toUpperCase());
+// change in Title Case
+let titleCaseName = "abDulLah";
+console.log(lowerCaseName.charAt(0).toUpperCase() + titleCaseName.slice(1, titleCaseName.length).toLowerCase());
--- src/day1.ts
+++ src/day1.ts
@@ -1,1 +1,25 @@
-console.log("Day 1");
+////////////////////////////////////////////    QUESTION 2  //////////////////////////////////////////
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+////////////////////////////////////////////     ANSWER  //////////////////////////////////////////
+let studentName: string = "Muhammad Adil"
+console.log(`“Hello ${studentName},would you like to learn some TypeScript today?”`);
+
+
+////////////////////////////////////////////    QUESTION 3  //////////////////////////////////////////
+//Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+////////////////////////////////////////////     ANSWER  //////////////////////////////////////////
+// change in lower Case
+let lowerCaseName: string = "ABDULLAH"
+console.log(lowerCaseName.toLowerCase());
+
+// change in upper Case
+let upperCaseName: string = "abdullah"
+console.log("",upperCaseName.toUpperCase());
+
+// change in Title Case
+let titleCaseName: string = "abDulLah"
+console.log(lowerCaseName.charAt(0).toUpperCase() + titleCaseName.slice(1, titleCaseName.length).toLowerCase());
+
+
