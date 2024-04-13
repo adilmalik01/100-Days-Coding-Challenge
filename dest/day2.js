37ef91aa79d052acc6f970ddd938c94830734ed8
adilmalik01 <malikadil5679@gmai.com>
2024-04-13 22:05:35 +0500

    Day 2 compelete

--- dest/day2.js
+++ dest/day2.js
@@ -1,3 +1,20 @@
 "use strict";
+////////////////////////////////////////////    QUESTION 4  //////////////////////////////////////////
+// Question 4: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// "Albert Einstein once said,A person who never made a mistake never tried anything new."
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("day 2");
+////////////////////////////////////////////      ANSWER  //////////////////////////////////////////
+console.log('Quaid e Azam once said, “I do not believe in taking the right decision, I take a decision and make it right.”');
+////////////////////////////////////////////    QUESTION 5  //////////////////////////////////////////
+// Question 5: Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+////////////////////////////////////////////      ANSWER  //////////////////////////////////////////
+let qoutePersonName = "Quaid e Azam";
+let quote = " “I do not believe in taking the right decision, I take a decision and make it right.”";
+let famousQuote = `${qoutePersonName},Once Said${quote}`;
+console.log(famousQuote);
+////////////////////////////////////////////    QUESTION 6  //////////////////////////////////////////
+// Question 6: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+////////////////////////////////////////////      ANSWER  //////////////////////////////////////////
+let personName = "Muhammad Adil";
+console.log("with WhiteSpace Name :", `\t\t\t${personName}`);
+console.log("Remove WhiteSpace in Name :", `${personName.trim()}`);
