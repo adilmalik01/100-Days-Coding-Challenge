7d94067971d9ffe9c9600c9a2002f93fea35153a
adilmalik01 <malikadil5679@gmai.com>
2024-04-13 22:08:34 +0500

    complete day 3

--- src/day3.ts
+++ src/day3.ts
@@ -1,1 +1,37 @@
-console.log("day 3");
+////////////////////////////////////////////    QUESTION 7  //////////////////////////////////////////
+// Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+
+////////////////////////////////////////////   ANSWER  //////////////////////////////////////////
+console.log(2 + 6);  
+console.log(13 - 5);
+console.log(2 * 4);
+console.log(32 / 4);
+
+
+////////////////////////////////////////////    QUESTION 8  //////////////////////////////////////////
+// Question 8: You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+////////////////////////////////////////////      ANSWER  //////////////////////////////////////////
+let num1: number = 4;
+let num2: number = 4;
+console.log(num1 + num2);
+
+console.log(4 + 4);
+
+let eight: number = 8;
+console.log(eight);
+
+
+
+////////////////////////////////////////////    QUESTION   9  //////////////////////////////////////////
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+
+
+////////////////////////////////////////////      ANSWER  //////////////////////////////////////////
+let favoriteNumber: number = 5
+console.log(`Hey guy's ${favoriteNumber} is my favorite number `);
+
+
