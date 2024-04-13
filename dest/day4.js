cfb1c1b94aaae4c04dc1087cb6fa0b1afd57bb67
adilmalik01 <malikadil5679@gmai.com>
2024-04-13 22:09:47 +0500

    day 4 complete

---
+++ dest/day4.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+////////////////////////////////////////////    QUESTION 10  //////////////////////////////////////////
+// Question 10: Adding Comments:  If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does
+////////////////////////////////////////////    ANSWER 10   //////////////////////////////////////////
+// Here I stored My Favourite Number in a varibale the name or favoutriteNumber And simply print 
+//  console.log with templete litterls and another text .
+let favoriteNumber = 5;
+console.log(`Hey guy's ${favoriteNumber} is my favorite number `);
+////////////////////////////////////////////    QUESTION 11  //////////////////////////////////////////
+// Question 11: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
+////////////////////////////////////////////    ANSWER 11   //////////////////////////////////////////
+let friendsName = ["Ali", "Azeem", "Feroz", "Talal", "Asif"];
+for (let i = 0; i < friendsName.length; i++) {
+    console.log(friendsName[i]);
+}
+// here I using For in loop
+console.log(" \n with For of loop \n");
+for (const i of friendsName) {
+    console.log(i);
+}
+////////////////////////////////////////////    QUESTION 12  //////////////////////////////////////////
+// Question 12: Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. The message should be the same for each person, but personalized with their name.
+////////////////////////////////////////////    ANSWER 12   //////////////////////////////////////////
+console.log("\n");
+friendsName.forEach((name) => {
+    console.log(`hello ${name} How are You brother`);
+});
